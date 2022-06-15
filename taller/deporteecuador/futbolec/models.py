e83ab1e1c32a3c4f91ec5df77c33118072d962e7
stevejr.23 <stevej4r412@hotmial.com>
2022-06-15 00:50:53 -0500

    clase

--- taller/deporteecuador/futbolec/models.py
+++ taller/deporteecuador/futbolec/models.py
@@ -2,57 +2,55 @@ from django.db import models
 
 # Create your models here.
 
+
 class Equipo(models.Model):
-    
+
     nombre = models.CharField(max_length=30)
     siglas = models.CharField(max_length=30, blank=True)  # el campo puede                                                        # ser vacio
     twitter = models.CharField(max_length=30, unique=True)
-    campeonatos = models.ManyToManyField('Campeonato', through='CampeonatosEquipos')
-
+    campeonatos = models.ManyToManyField(
+        'Campeonato', through='CampeonatosEquipo')
 
     def __str__(self):
-        return "%s - siglas: %s - twitter: %s" % (
-                self.nombre,
-                self.siglas,
-                self.twitter)
-   
-    # estudiantes = models.ManyToManyField(Estudiante, through='Matricula')
+        return "Equipo: Nombre:%s - Siglas:%s - Twitter:%s" % (
+            self.nombre,
+            self.siglas,
+            self.twitter)
 
 
 class Jugador(models.Model):
-    """
-    """
-    nombre = models.CharField(max_length=30) 
-    posicion = models.CharField(max_length=30) 
-    numero = models.IntegerField("Numero Camiseta") 
-    sueldo = models.FloatField("Sueldo") 
-    equipo = models.ForeignKey(Equipo, related_name='equipos', 
-            on_delete=models.CASCADE)
- 
+    nombre = models.CharField(max_length=30)
+    posicion = models.CharField(max_length=30)
+    numero = models.IntegerField('Numero Camiseta')
+    sueldo = models.FloatField('Sueldo')
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE,
+                               related_name="equipo")
+
     def __str__(self):
-        return "%s - posicion: %s - dorsal: %d - sueldo: %d - Equipo(%s)" % (
-                self.nombre,
-                self.posicion,
-                self.numero,
-                self.sueldo,
-                self.equipo)
+        return "Jugador:Nombre: %s - Posicion: %s - Dorsal: %d \
+        Sueldo: %d - Equipo(%s)" % (
+            self.nombre,
+            self.posicion,
+            self.numero,
+            self.sueldo,
+            self.equipo)
 
 
 class Campeonato(models.Model):
-    """
-    """
-    nombre = models.CharField(max_length=30) 
-    auspiciante = models.CharField(max_length=30)
-    equipo = models.ManyToManyField('Equipo', through='CampeonatosEquipos')
 
+    nombre = models.CharField(max_length=30)
+    auspiciante = models.CharField(max_length=30)
+
+    equipos = models.ManyToManyField('Equipo', through='CampeonatosEquipo')
 
     def __str__(self):
-        return "Campeonato: %s - %s" % \
-                (self.nombre, self.auspiciante)
+        return "Campeonato: Nombre:%s - Auspiciante:%s" % (
+            self.nombre,
+            self.auspiciante)
 
-class CampeonatosEquipos(models.Model):
-    """
-    """
+
+class CampeonatosEquipo(models.Model):
+
     anio = models.IntegerField("Año")
     equipo = models.ForeignKey(Equipo, related_name='losequipos', 
             on_delete=models.CASCADE)
@@ -62,4 +60,3 @@ class CampeonatosEquipos(models.Model):
     def __str__(self):
         return "Informacion: Año(%d) - Equipo(%s) - Campeonato(%s)" % \
                 (self.anio, self.equipo.nombre,self.campeonato.nombre)
-    
